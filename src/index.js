7975435655f086c4dde717628eb580a76909df5a
prakash <prakash.bokati@seelogic.co.uk>
2019-07-02 15:34:32 +0545

    refactored promises

--- src/index.js
+++ src/index.js
@@ -8,77 +8,71 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch(error => {
-      res.status(400).send(error);
-    });
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.post('/futsals', (req, res) => {
+app.post('/futsals', async (req, res) => {
   const futsal = new Futsal(req.body);
-  futsal
-    .save()
-    .then(() => {
-      res.status(201).send(futsal);
-    })
-    .catch(error => {
-      res.status(400).send(error);
-    });
+  try {
+    await futsal.save();
+    res.status(201).send(futsal);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
-app.get('/users', (req, res) => {
-  User.find({})
-    .then(users => res.send(users))
-    .catch(error => {
-      res.status(500).send(error);
-    });
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
-  User.findById(_id)
-    .then(user => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    });
+  try {
+    const user = await User.findById({ _id });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get('/futsals', (req, res) => {
-  Futsal.find({})
-    .then(futsals => {
-      if (!futsals) {
-        return res.status(404).send();
-      }
-      res.send(futsals);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    });
+app.get('/futsals', async (req, res) => {
+  try {
+    const futsals = await Futsal.find({});
+    if (!futsals) {
+      return res.status(404).send();
+    }
+    res.send(futsals);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get('/futsals/:id', (req, res) => {
+app.get('/futsals/:id', async (req, res) => {
   const _id = req.params.id;
-  Futsal.findById(_id)
-    .then(futsal => {
-      if (!futsal) {
-        return res.status(404).send();
-      }
-      res.send(futsal);
-    })
-    .catch(error => {
-      res.status(500).send(error);
-    });
+  try {
+    const futsal = await Futsal.findById(_id);
+    if (!futsal) {
+      return res.status(404).send();
+    }
+    res.send(futsal);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(port, () => {
