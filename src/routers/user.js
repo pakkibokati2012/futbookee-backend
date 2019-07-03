a901f73060af7adb54da3c9fda549df706ff4f3d
prakash <prakash.bokati@seelogic.co.uk>
2019-07-03 15:49:35 +0545

    wrote auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const Futsal = require('../models/futsal');
+const auth = require('../middlewares/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -14,13 +15,8 @@ router.post('/users', async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
