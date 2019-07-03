43681dd2ebe23e764ee0999803b125cef37b6d94
prakash <prakash.bokati@seelogic.co.uk>
2019-07-03 14:18:35 +0545

    generated tokens and sent adding to the response

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,7 +39,15 @@ const userSchema = new mongoose.Schema({
         throw new Error('Password cannot contain "password"');
       }
     }
-  }
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 userSchema.statics.findByCredentials = async (email, password) => {
@@ -57,6 +66,14 @@ userSchema.statics.findByCredentials = async (email, password) => {
   }
 
   return user;
+};
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'futbookee');
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
 };
 
 // Using standard function as this binding plays important role
--- src/routers/user.js
+++ src/routers/user.js
@@ -7,7 +7,8 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -82,7 +83,8 @@ router.post('/users/login', async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
