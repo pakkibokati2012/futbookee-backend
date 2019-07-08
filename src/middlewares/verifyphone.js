d2ada23b7215d7556a83467ffab0a2cf312bef71
prakash <prakash.bokati@seelogic.co.uk>
2019-07-08 13:22:20 +0545

    phone authentication works but messed validation

--- src/middlewares/verifyphone.js
+++ src/middlewares/verifyphone.js
@@ -5,15 +5,19 @@ const client = require('twilio')(accountSid, authToken);
 const verifyPhoneNumber = async (req, res, next) => {
   try {
     const phoneNumber = req.body.phone;
-    client.verify.services('VAec698bdac64fe79b555e0f91d2ce307b')
-      .verificationChecks
-      .create({to: req.body.phone, code: req.body.code.toString()})
+    client.verify
+      .services('VAec698bdac64fe79b555e0f91d2ce307b')
+      .verificationChecks.create({
+        to: req.body.phone,
+        code: req.body.code.toString()
+      })
       .then(verification_check => {
-          if (verification_check.status === 'approved') {
-                    return next();
-          }
-          throw new Error('You are not authorized yet');
-          });
+        if (verification_check.valid === true) {
+          return next();
+        }
+        throw new Error('Provided code is not valid!');
+      })
+      .catch(error => res.status(401).send(error));
   } catch (error) {
     res.status(401).send('Could not verify phone number');
   }
